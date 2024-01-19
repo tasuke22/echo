38e2c3c6f9cabc908960d60d96e65e8127178390
tasuke22 <tasukesuke@gmail.com>
2024-01-19 09:01:37 +0900

    add main

--- main.go
+++ main.go
@@ -1,8 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"github.com/tasuke/udemy/controller"
+	"github.com/tasuke/udemy/db"
+	"github.com/tasuke/udemy/repository"
+	"github.com/tasuke/udemy/router"
+	"github.com/tasuke/udemy/usecase"
+)
 
 func main() {
-	fmt.Println("Hello World")
-	fmt.Println("Hello World")
+	// ここから連続で依存性の注入。外側でインスタンス化したものを注入
+	// IFが返ってきてそれを注入しているので疎結合
+	db := db.NewDB()
+	userRepository := repository.NewUserRepository(db)
+	userUsecase := usecase.NewUserUsecase(userRepository)
+	userController := controller.NewUserController(userUsecase)
+	e := router.NewRouter(userController)
+	// エラーが発生した場合はログ情報を出力してプログラムを強制終了
+	e.Logger.Fatal(e.Start(":8080"))
 }
